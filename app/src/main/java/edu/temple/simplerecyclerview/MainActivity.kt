04d99a2519708d233645047e3cb8a91d8f6cc65b
Malvin Prifti <mprifti34@gmail.com>
2023-09-26 15:21:17 -0400

    Implemented RecyclerView

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -1,20 +1,27 @@
 package edu.temple.simplerecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val numbers = Array(50) {it}
+
         // Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Step 2: Provide a LayoutManager
-
+        val gridLayoutManager = GridLayoutManager(this, 4)
+        recyclerView.layoutManager = gridLayoutManager
 
         //Step 4: Provide a RecyclerView.Adapter
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
+
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,33 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter (_numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+
+    private val numbers = _numbers
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
-    //Step 3b: Complete function definitions for adapter
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NumberDisplayAdapter.NumberViewHolder {
+        return NumberViewHolder(
+            TextView(parent.context).apply{
+                layoutParams = ViewGroup.LayoutParams(300, 600)
+            }
+        )
+    }
 
+    override fun onBindViewHolder(holder: NumberDisplayAdapter.NumberViewHolder, position: Int) {
+        holder.textView.setText(numbers[position])
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
 }
